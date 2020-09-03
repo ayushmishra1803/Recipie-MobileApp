80e1e9dc90953822c08ae12541020588b958d1cd
khushboothakur2305 <khushboothakur2305@gmail.com>
2020-09-03 19:57:33 +0530

    update

--- src/app/home/foodsearch/foodsearch.page.ts
+++ src/app/home/foodsearch/foodsearch.page.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-foodsearch',
-  templateUrl: './foodsearch.page.html',
-  styleUrls: ['./foodsearch.page.scss'],
+  selector: "app-foodsearch",
+  templateUrl: "./foodsearch.page.html",
+  styleUrls: ["./foodsearch.page.scss"],
 })
 export class FoodsearchPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  onSubmit() {
+    console.log("hello");
   }
-
 }
