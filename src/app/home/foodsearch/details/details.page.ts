debc6385692b0c1cd28f9a6d6977656f0b8a8141
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-09-03 20:12:44 +0530

    Updates

--- src/app/home/foodsearch/details/details.page.ts
+++ src/app/home/foodsearch/details/details.page.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Observable, Subscription } from "rxjs";
+import { FoodDatabaseService } from "./../../../service/foodDataBase/food-database.service";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-details',
-  templateUrl: './details.page.html',
-  styleUrls: ['./details.page.scss'],
+	selector: "app-details",
+	templateUrl: "./details.page.html",
+	styleUrls: ["./details.page.scss"],
 })
 export class DetailsPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+	constructor(private service: FoodDatabaseService) {}
+  dataobs = new Observable();
+  DataArray:any;
+  dataSub=new Subscription;
+	ngOnInit() {
+    this.dataobs = this.service.getFoodFromDatabase();
+    this.dataSub=this.dataobs.subscribe(re=>{
+      console.log(re + "relog");
+      this.DataArray=re;
+      console.log(this.DataArray);
+      
+    })
+	}
 }
