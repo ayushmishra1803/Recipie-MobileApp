ce1e52711466d26d0e6061b8704c332aa2278fb5
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-09-03 20:09:57 +0530

    uPDATES

--- src/app/home/foodsearch/foodsearch.page.ts
+++ src/app/home/foodsearch/foodsearch.page.ts
@@ -1,18 +1,20 @@
-import { FoodDatabaseService } from './../../service/foodDataBase/food-database.service';
+import { Router } from "@angular/router";
+import { FoodDatabaseService } from "./../../service/foodDataBase/food-database.service";
 import { Component, OnInit } from "@angular/core";
-import { NgForm } from '@angular/forms';
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: "app-foodsearch",
-  templateUrl: "./foodsearch.page.html",
-  styleUrls: ["./foodsearch.page.scss"],
+	selector: "app-foodsearch",
+	templateUrl: "./foodsearch.page.html",
+	styleUrls: ["./foodsearch.page.scss"],
 })
 export class FoodsearchPage implements OnInit {
-  constructor(private service:FoodDatabaseService) {}
+	constructor(private service: FoodDatabaseService, private Router: Router) {}
 
-  ngOnInit() {}
-  onSubmit(f:NgForm) {
-    console.log("hello");
-    this.service.setIngredients(f.value.searched);
-  }
+	ngOnInit() {}
+	onSubmit(f: NgForm) {
+		console.log("hello");
+		this.service.setIngredients(f.value.searched);
+		this.Router.navigate(["/home/tabs/foodsearch/details"]);
+	}
 }
