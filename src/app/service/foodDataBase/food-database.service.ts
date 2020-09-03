e32cc4f400c864dafd43e5138030e2dc3a78a900
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-09-03 23:55:10 +0530

    Updatse

--- src/app/service/foodDataBase/food-database.service.ts
+++ src/app/service/foodDataBase/food-database.service.ts
@@ -10,7 +10,7 @@ export class FoodDatabaseService {
 	private FoodDatabseApp_id = "4a81b8a2";
 	private FoodDatabseKEy = "b90f3fdd1001327868c2e516d83f8ef6";
 	 Ingredients: string ;
-
+	Details:any[]
 	setIngredients(data) {
 		console.log(data);
 		
@@ -25,5 +25,11 @@ export class FoodDatabaseService {
 		return this.http.get<{ hints: any[]; parsed:any[] }>(
 			`https://api.edamam.com/api/food-database/v2/parser?ingr=${this.Ingredients}&app_id=${this.FoodDatabseApp_id}&app_key=${this.FoodDatabseKEy}`,
 		);
+	}
+	setDetails(data:any[]){
+		this.Details=data
+	}
+	getDetails(){
+		return this.Details
 	}
 }
