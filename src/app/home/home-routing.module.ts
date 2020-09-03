dddaec29a55c953a11da1e57b8a2175a00d2b7f8
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-09-03 19:39:14 +0530

    Updates

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -3,9 +3,7 @@ import { RouterModule, Routes } from "@angular/router";
 import { HomePage } from "./home.page";
 const routes: Routes = [
 	{
-		path: "",
-
-		redirectTo: "tabs/foodsearch",
+		path: "",redirectTo: "tabs/foodsearch",
 	},
 	{
 		path: "tabs",
