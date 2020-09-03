8c3e3986f7c7cc8b3d521e2a4d7fcb6b55117e12
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-09-04 00:00:12 +0530

    Updates

--- src/app/home/foodsearch/details/fooddetails/fooddetails.page.ts
+++ src/app/home/foodsearch/details/fooddetails/fooddetails.page.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { FoodDatabaseService } from "./../../../../service/foodDataBase/food-database.service";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-fooddetails',
-  templateUrl: './fooddetails.page.html',
-  styleUrls: ['./fooddetails.page.scss'],
+	selector: "app-fooddetails",
+	templateUrl: "./fooddetails.page.html",
+	styleUrls: ["./fooddetails.page.scss"],
 })
 export class FooddetailsPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+	constructor(private service: FoodDatabaseService) {}
+	data: any[];
+	ngOnInit() {
+		this.data = this.service.getDetails();
+	}
 }
