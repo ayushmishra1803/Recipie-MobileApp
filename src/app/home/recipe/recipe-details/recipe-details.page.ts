90fc498d795e5a4fdcfd7c457f6dc5d825a4882a
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-09-04 02:23:59 +0530

    uPDATES

--- src/app/home/recipe/recipe-details/recipe-details.page.ts
+++ src/app/home/recipe/recipe-details/recipe-details.page.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Subscription } from "rxjs";
+import { Observable } from "rxjs";
+import { LoadingController } from "@ionic/angular";
+import { RecipeService } from "./../../../service/recipe/recipe.service";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-recipe-details',
-  templateUrl: './recipe-details.page.html',
-  styleUrls: ['./recipe-details.page.scss'],
+	selector: "app-recipe-details",
+	templateUrl: "./recipe-details.page.html",
+	styleUrls: ["./recipe-details.page.scss"],
 })
 export class RecipeDetailsPage implements OnInit {
+	constructor(
+		private service: RecipeService,
+		private controler: LoadingController,
+	) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+	DataObs = new Observable();
+	DataSub = new Subscription();
+	DataArray: any;
+	ngOnInit() {
+		this.controler
+			.create({
+				keyboardClose: true,
+				message: "Recipe is on the way Please Wait.......",
+			})
+			.then((loader) => {
+				loader.present();
+				this.DataObs = this.service.getRecipe();
+				this.DataSub = this.DataObs.subscribe((re) => {
+					console.log(re);
+					this.DataArray = re;
+          console.log(this.DataArray);
+          loader.dismiss();
+				});
+			});
+	}
 }
--- src/app/service/recipe/recipe.service.ts
+++ src/app/service/recipe/recipe.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -7,8 +8,13 @@ export class RecipeService {
 	private App_Id = "8064ea95";
   private App_Key = "ddf7ba4dbd5b106269e7466af267ce12";
   Searched:string="";
-  constructor() {}
+  constructor(private http:HttpClient) {}
   serSearched(data:string){
     this.Searched=data;
+  }
+  getRecipe(){
+    return this.http.get(
+			`https://api.edamam.com/search?q=${this.Searched}&app_id=${this.App_Id}&app_key=${this.App_Key}`,
+		);
   }
 }
