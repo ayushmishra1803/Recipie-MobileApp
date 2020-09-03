f2d9e6fd0e8b9e287d1969c29f11a9f463a2a3fd
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-09-03 23:58:05 +0530

    Updates

--- src/app/home/foodsearch/details/details.page.ts
+++ src/app/home/foodsearch/details/details.page.ts
@@ -1,3 +1,4 @@
+import { Router } from "@angular/router";
 import { Observable, Subscription } from "rxjs";
 import { FoodDatabaseService } from "./../../../service/foodDataBase/food-database.service";
 import { Component, OnInit } from "@angular/core";
@@ -8,7 +9,7 @@ import { Component, OnInit } from "@angular/core";
 	styleUrls: ["./details.page.scss"],
 })
 export class DetailsPage implements OnInit {
-	constructor(private service: FoodDatabaseService) {}
+	constructor(private service: FoodDatabaseService, private router: Router) {}
 	dataobs = new Observable<{ hints: any[]; parsed: any[] }>();
 	DataHint: any[] = [];
 	dataSub = new Subscription();
@@ -25,7 +26,8 @@ export class DetailsPage implements OnInit {
 			console.log(this.DataHint);
 		});
 	}
-	details(data:any[]){
-
+	details(data: any[]) {
+		this.service.setDetails(data);
+		this.router.navigate(["//home/tabs/foodsearch/details/fooddetails"]);
 	}
 }
